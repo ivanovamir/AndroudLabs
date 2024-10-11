b7fd92941ed2ab75e218d9e1a303ba3d0cb267bb
ivanovamir <adamstradvers@gmail.com>
2024-10-11 20:11:03 +0300

    Unit 2 Pathway 1 fifth commit song

---
+++ KotlinPathways/Unit2Pathway1/app/src/main/java/com/example/unit2pathway1/Practice5.kt
@@ -0,0 +1,33 @@
+package com.example.unit2pathway1
+
+fun main() {
+
+    val amanda = Person("Amanda", 33, "play tennis", null)
+    val atiqah = Person("Atiqah", 28, "climb", amanda)
+
+    amanda.showProfile()
+    atiqah.showProfile()
+}
+
+
+class Person(private val name: String, private val age: Int, private val hobby: String?,
+             private val referrer: Person?) {
+    fun showProfile() {
+        println("Name: $name")
+        println("Age: $age")
+        if(hobby != null) {
+            print("Likes to $hobby. ")
+        }
+        if(referrer != null) {
+            print("Has a referrer named ${referrer.name}")
+            if(referrer.hobby != null) {
+                print(", who likes to ${referrer.hobby}.")
+            } else {
+                print(".")
+            }
+        } else {
+            print("Doesn't have a referrer.")
+        }
+        print("\n\n")
+    }
+}
