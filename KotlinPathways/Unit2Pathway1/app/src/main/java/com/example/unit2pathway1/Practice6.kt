cba4a40de5cc68a1029946251247fd6706da0a82
ivanovamir <adamstradvers@gmail.com>
2024-10-12 09:35:11 +0300

    Unit 2 Pathway 1 sixth commit fold

---
+++ KotlinPathways/Unit2Pathway1/app/src/main/java/com/example/unit2pathway1/Practice6.kt
@@ -0,0 +1,50 @@
+package com.example.unit2pathway1
+
+open class Phone(var isScreenLightOn: Boolean = false){
+    open fun switchOn() {
+        isScreenLightOn = true
+    }
+
+    fun switchOff() {
+        isScreenLightOn = false
+    }
+
+    fun checkPhoneScreenLight() {
+        val phoneScreenLight = if (isScreenLightOn) "on" else "off"
+        println("The phone screen's light is $phoneScreenLight.")
+    }
+}
+
+class FoldablePhone(var isFolded: Boolean = true): Phone() {
+    override fun switchOn() {
+        if (!isFolded) {
+            isScreenLightOn = true
+        }
+    }
+
+    fun fold() {
+        isFolded = true
+    }
+
+    fun unfold() {
+        isFolded = false
+    }
+}
+
+fun main() {
+    val newFoldablePhone = FoldablePhone(isFolded = false)
+
+    newFoldablePhone.switchOn()
+    newFoldablePhone.checkPhoneScreenLight()
+    newFoldablePhone.unfold()
+    newFoldablePhone.switchOn()
+    newFoldablePhone.checkPhoneScreenLight()
+
+    val newUnFoldablePhone = FoldablePhone(isFolded = true)
+
+    newUnFoldablePhone.switchOn()
+    newUnFoldablePhone.checkPhoneScreenLight()
+    newUnFoldablePhone.unfold()
+    newUnFoldablePhone.switchOn()
+    newUnFoldablePhone.checkPhoneScreenLight()
+}
